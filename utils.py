514ce9127b9bb092365e377e60299e7caba2d496
Pedro Henrique <pedrohenriquecoutinhodeoliveir@gmail.com>
2025-06-14 21:24:31 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,20 @@
-import streamlit as st
+import pandas as pd
+import os
 
-def carregar_imagem():
-    st.markdown(
-        """
-        <style>
-        .stApp {
-            background-image: url("https://i.imgur.com/Qf6uIuR.png");
-            background-size: cover;
-        }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
+def carregar_historico():
+    if not os.path.exists("historicos/sinais.csv"):
+        return pd.DataFrame(columns=["Ativo", "Tipo", "Entrada", "Saída", "Tendência"])
+    return pd.read_csv("historicos/sinais.csv")
+
+def salvar_sinal(sinal):
+    df = carregar_historico()
+    df = pd.concat([df, pd.DataFrame([sinal])], ignore_index=True)
+    df.to_csv("historicos/sinais.csv", index=False)
+
+def exibir_historico():
+    df = carregar_historico()
+    if df.empty:
+        st.info("Nenhum sinal gerado ainda.")
+    else:
+        st.table(df.tail(10))
+        
