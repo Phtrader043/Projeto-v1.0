284acae823f683337613aef5f40ff34338ae0df6
Pedro Henrique <pedrohenriquecoutinhodeoliveir@gmail.com>
2025-06-14 21:19:22 -0300

    Update signal_engine.py

--- signal_engine.py
+++ signal_engine.py
@@ -1,8 +1,24 @@
-
-from cohere_analysis import analisar_tendencia
 from data_fetcher import obter_dados
+from cohere_analysis import analisar_tendencia
+from datetime import datetime, timedelta
+import pytz
 
 def gerar_sinal(modo):
     dados = obter_dados()
+
     analise = analisar_tendencia(dados, modo)
-    return analise
+
+    agora = datetime.now(pytz.timezone('America/Sao_Paulo'))
+    entrada = (agora + timedelta(minutes=2)).strftime("%H:%M")
+    saida = (agora + timedelta(minutes=7)).strftime("%H:%M")
+
+    sinal = {
+        "Ativo": analise["ativo"],
+        "Tipo": analise["tipo"],
+        "Entrada": entrada,
+        "Saída": saida,
+        "Tendência": analise["confianca"]
+    }
+
+    return sinal
+    
